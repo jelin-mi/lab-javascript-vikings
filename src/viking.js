4f958a7cd8d461881e81a6c9756d7db20c4ca018
“jelin.mi” <jelin.mi@gmail.com>
2021-10-24 21:49:54 +0200

    missing vikingAttack and saxonAttack

--- src/viking.js
+++ src/viking.js
@@ -15,6 +15,7 @@ class Soldier {
 
 }
 
+
 // Viking
 class Viking extends Soldier {
   constructor (name, health, strength) {
@@ -39,10 +40,74 @@ class Viking extends Soldier {
 
 
 // Saxon
-class Saxon {}
+class Saxon extends Soldier {
+  constructor(health, strength) {
+    super (health, strength);
+  }
+  receiveDamage(damage){
+    this.health = this.health - damage;
+    if (this.health > 0){
+      return `A Saxon has received ${damage} points of damage`; 
+    } else {
+      return `A Saxon has died in combat`;
+    }
+  }
+
+}
+
 
 // War
-class War {}
+class War {
+  constructor() {
+    this.vikingArmy = [];
+    this.saxonArmy = [];
+  }
+  addViking(Viking){
+    this.vikingArmy.push(Viking);
+  }
+  
+  addSaxon(Saxon){
+    this.saxonArmy.push(Saxon);
+  }
+  
+  vikingAttack(){
+// Saxon.receiveDamage = function { saxonArmy[Saxon.health] -= vikingArmy[Viking.strength]}
+
+// this.saxonArmy[0].receiveDamage(this.vikingArmy[0].strength) 
+
+// Y si en ese array pones Math.floor(this.saxonArmy * Math.random)?
+
+
+  this.saxonArmy[0].receiveDamage(this.vikingArmy[0].strength);
+  this.saxonArmy.health === 0;
+  this.saxonArmy.shift(Saxon);
+
+
+
+    /* if (Saxon.damage = Viking.strength){
+      Saxon.receiveDamage();
+    }
+    
+    if (Saxon.health <= 0){
+      this.saxonArmy.pop(Saxon);
+    } */
+  }
+  
+  saxonAttack(){}
+  
+  
+  showStatus(){
+    if (this.saxonArmy.length === 0){
+      return "Vikings have won the war of the century!";
+    } else if (this.vikingArmy.length === 0){
+      return "Saxons have fought for their lives and survived another day...";
+    } else if (this.saxonArmy.length >= 1 && this.vikingArmy.length >= 1){
+      return "Vikings and Saxons are still in the thick of battle.";
+    }
+  }
+
+}
+
 
 
 
